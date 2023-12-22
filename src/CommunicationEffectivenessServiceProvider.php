dd633f5c4d3d364cbda8197c814f8683d6adf8af
Bruno Falcão <bruno.c.falcao@brunofalcao.me>
2023-12-22 15:00:50 +0100

    Update CommunicationEffectivenessServiceProvider.php

--- src/CommunicationEffectivenessServiceProvider.php
+++ src/CommunicationEffectivenessServiceProvider.php
@@ -8,9 +8,13 @@ class CommunicationEffectivenessServiceProvider extends EdukaServiceProvider
 {
     public function boot()
     {
+        Vite::macro('image', fn (string $asset) => $this->asset("resources/assets/images/{$asset}"));
+
+        Vite::useBuildDirectory('vendor/' . Nereus::course()->canonical);
+
         $this->customViewNamespace(__DIR__.'/../resources/views', 'course');
 
-        $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
+        $this->loadMigrationsFrom(__DIR__ . '/../database/migrations');
 
         parent::boot();
     }
